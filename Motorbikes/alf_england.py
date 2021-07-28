8b1e11d6f40f33f8d8dabd469b8debb520a1c545
kuroazai <pyromwansa@gmail.com>
2021-07-28 17:39:33 +0100

    Update alf_england.py

--- Motorbikes/alf_england.py
+++ Motorbikes/alf_england.py
@@ -13,8 +13,7 @@ urllib3.disable_warnings()
 http = urllib3.PoolManager()
 
 
-def get_prices(soup):
-    # Fine the price
+def get_details(soup):
     divs = soup.findAll("div", {"class": "grid-container"})
     span = re.findall('"bike-price">([^"]*)<', str(divs))
     price = re.sub('\D', '', span[0])
@@ -22,20 +21,14 @@ def get_prices(soup):
     price = price[1-len(price):]
     print "Price : £", price
 
-    # Find specs
     specs = soup.findAll("div", {"class": "bike-specification"})
-    #specs = list(filter(None, specs))    
-    #specs = [x for x in specs if x]
-    i = 0
     e_specs = []
     for x in specs[0]:
-        if None == x.name:
+        if x.name:
             pass
         else:
-            #print i, x, "\n"
             e_specs.append(x)
-            i += 1
-    #print len(e_specs)
+
     # Find Weight
     span = re.findall('>([^"]*)kg', str(specs))
     print "Weight : ", span[len(span)-1].strip(), "KG"
@@ -46,7 +39,6 @@ def get_prices(soup):
         a = str(m_hp[0]).split(",")
         a = re.sub('\D', '', a[1])
         m_hp = re.sub('\D', '', str(m_hp[0]))
-        #print a[:-1], len(a)
         m_hp = m_hp[:len(m_hp) / 2 + 1]
         if len(a) > 2:
             hp = float(a[:-1]) / 1.014
@@ -61,21 +53,19 @@ def get_prices(soup):
     else:
         print "Horsepower Unavaliable"
 
-    #Find Displacement 
+    # Find Displacement
     dpc = re.findall('>([^"]*)cc', str(e_specs[1]))
     print "Displacement : ", dpc[0], "CC"
-    
-    #Find Torque & peak
+
+    # Find Torque & peak
     trq = re.findall('>([^"]*) Nm', str(e_specs[5]))
-    if len(trq) >= 1:    
+    if len(trq) >= 1:
         print "Torque : ", trq[0], "Nm"
-        peak_t = re.findall('(@([^"]*))rpm', str(e_specs[5]))   
+        peak_t = re.findall('(@([^"]*))rpm', str(e_specs[5]))
         b = str(peak_t[0]).split()
         print "Peak Torque @ ", b[1], "RPM"
     else:
         print "Torque Unavaliable"
-def save():
-    pass
 
 
 # Individual Containers for the names of the bikes
@@ -85,25 +75,17 @@ Mt = ['MT-10-SP', 'MT-10', 'MT-07', 'MT-03', 'MT-125']
 # Joint container
 bikes = Yzf + Mt
 
-# base url
-url = "https://www.alfengland.co.uk/new-yamaha-bikes/asset/" + bikes[4]
-#********************************************************************
-#   Beautiful Soup
-#********************************************************************
+
 response = http.request('GET', url)
 soup = BeautifulSoup(response.data, "html.parser")
 
 for x in bikes:
     print x
-
     # Build Url
     url = "https://www.alfengland.co.uk/new-yamaha-bikes/asset/" + x
     print url
     # Soup
     response = http.request('GET', url)
     soup = BeautifulSoup(response.data, "html.parser")
-    get_prices(soup)
+    get_details(soup)
     print "\n\n"
-
-raw_input()
-
